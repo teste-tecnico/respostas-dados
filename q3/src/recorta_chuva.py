e932579ebf4017802876f3bc6a36525e32e601ad
Ramonmurilo <ramonmurilo@id.uff.br>
2021-09-30 01:14:52 -0300

    Q3 : INCLUSÃO : Módulo para download do MERGE, conversão grib para nc, recorte bacia tietê

---
+++ q3/src/recorta_chuva.py
@@ -0,0 +1,32 @@
+import rioxarray
+import xarray as xr
+import geopandas as gpd
+
+def preparar_para_recorte(dataset:any, crs="epsg:4326", xdim="lon", ydim="lat") -> any:
+    """Sub-função de main().
+    Ajusta coordenadas e dados para o recorte da chuva.
+
+    Args:
+        dataset (dataset): Dado a ser recortado pelo shapefile
+        crs (str, optional): Projeção cartográfica. Defaults to "epsg:4326".
+        xdim (str, optional): Nome da dimensão "longitude" no dataset. Defaults to "lon".
+        ydim (str, optional): Nome da dimensão "latitude" no dataset. Defaults to "lat".
+
+    Returns:
+        dataset: Dados tratados para recorte
+    """
+    dataset = dataset.assign_coords(lon=(((dataset.lon + 180) % 360) - 180)).sortby(xdim)
+    dataset = dataset.rio.set_spatial_dims(x_dim=xdim, y_dim=ydim) 
+    dataset = dataset.rio.write_crs(crs)
+    return dataset
+
+
+def main(dataset:str,shapefile:str):
+
+    shp_bacia = gpd.read_file(shapefile)
+    #leitura, preparação e recorte dos dados
+    dados = xr.open_dataset(dataset)
+    dados_preparados = preparar_para_recorte(dados.prec)
+    dados_recortados = dados_preparados.rio.clip(shp_bacia["geometry"], "epsg:4326")
+
+    return dados_recortados
